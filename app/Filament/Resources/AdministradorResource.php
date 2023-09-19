d1692e00013ac9cd4e8d49a52b5575b25a8d35ec
Paulino Fonseca <paulinofonseca@gmail.com>
2023-09-19 23:00:18 +0100

    adding view admin

--- app/Filament/Resources/AdministradorResource.php
+++ app/Filament/Resources/AdministradorResource.php
@@ -5,14 +5,18 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\AdministradorResource\Pages\CreateAdministrador;
 use App\Filament\Resources\AdministradorResource\Pages\EditAdministrador;
 use App\Filament\Resources\AdministradorResource\Pages\ListAdministradors;
-use App\Models\EstadoDaConta;
+use App\Filament\Resources\AdministradorResource\Pages\ViewAdministrador;
 use App\Models\Administrador;
+use App\Models\EstadoDaConta;
 use App\Models\Municipio;
 use App\Models\Provincia;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Fieldset;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables\Table;
 use Rmsramos\Activitylog\RelationManagers\ActivitylogRelationManager;
@@ -22,6 +26,48 @@ class AdministradorResource extends Resource
     protected static ?string $model = Administrador::class;
 
     protected static ?string $navigationIcon = 'clarity-administrator-solid';
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                \Filament\Infolists\Components\Section::make('Informações do médico')
+                ->collapsible()
+                ->schema([
+                TextEntry::make('funcionario.user.name')
+                            ->label('Nome completo')
+                            ->size(TextEntry\TextEntrySize::Large)
+                            ->columnSpanFull(),
+                        TextEntry::make('funcionario.user.email')
+                            ->label('Email')
+                            ->size(TextEntry\TextEntrySize::Large),
+                        TextEntry::make('funcionario.telefone')
+                            ->label('Telefone')
+                            ->numeric(thousandsSeparator: ' '),
+                        TextEntry::make('funcionario.estadoDaConta.nome')
+                            ->label('Estado da conta')
+                            ->badge()
+                            ->colors([
+                                'success' => 'Activa',
+                                'danger' => 'Inativa',
+                                'warning' => 'Desactivada',
+                            ])
+                            ->size(TextEntry\TextEntrySize::Large),
+                        Fieldset::make('Localização')
+                            ->schema([
+                                TextEntry::make('funcionario.endereco.provincia.nome')
+                                    ->label('Provincia')
+                                    ->size(TextEntry\TextEntrySize::Large),
+                                TextEntry::make('funcionario.endereco.municipio.nome')
+                                    ->label('Municipio')
+                                    ->size(TextEntry\TextEntrySize::Large),
+                                TextEntry::make('funcionario.endereco.rua')
+                                    ->label('Rua')
+                                    ->size(TextEntry\TextEntrySize::Large),
+                            ])->columns(2),
+                    ])->columns(2),
+            ]);
+    }
 
     public static function form(Form $form): Form
     {
@@ -63,6 +109,7 @@ class AdministradorResource extends Resource
                                 }
 
                                 $munis = Municipio::where('provincia_id', '=', $provincia_id)->pluck('nome', 'id');
+
                                 return $munis;
                             })
                             ->searchable(),
@@ -110,9 +157,9 @@ class AdministradorResource extends Resource
                     ->dateTime(),
             ])
             ->filters([
-                //
             ])
             ->actions([
+                \Filament\Tables\Actions\ViewAction::make(),
                 \Filament\Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -133,6 +180,7 @@ class AdministradorResource extends Resource
             'index' => ListAdministradors::route('/'),
             'create' => CreateAdministrador::route('/create'),
             'edit' => EditAdministrador::route('/{record}/edit'),
+            'view' => ViewAdministrador::route('/{record}'),
         ];
     }
 }
