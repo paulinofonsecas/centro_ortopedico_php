851b8e2e8364baab1765f20aa1bc4ff1579bed0f
Paulino Fonseca <paulinofonseca@gmail.com>
2023-12-18 05:39:27 +0100

    feat: cria o Paciente resource

---
+++ app/Filament/Tecnico/Resources/PacienteResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Tecnico\Resources;
+
+use App\Filament\Tecnico\Resources\PacienteResource\Pages;
+use App\Filament\Tecnico\Resources\PacienteResource\RelationManagers;
+use App\Models\Paciente;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PacienteResource extends Resource
+{
+    protected static ?string $model = Paciente::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPacientes::route('/'),
+            'create' => Pages\CreatePaciente::route('/create'),
+            'edit' => Pages\EditPaciente::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Filament/Tecnico/Resources/PacienteResource/Pages/CreatePaciente.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Tecnico\Resources\PacienteResource\Pages;
+
+use App\Filament\Tecnico\Resources\PacienteResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePaciente extends CreateRecord
+{
+    protected static string $resource = PacienteResource::class;
+}
