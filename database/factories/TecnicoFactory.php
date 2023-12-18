8c2276966d228b05f1d916cd0dc205ef22498e2a
Paulino Fonseca <paulinofonseca@gmail.com>
2023-12-18 04:39:29 +0100

    feat: cria a tecnico factory

--- database/factories/TecnicoFactory.php
+++ database/factories/TecnicoFactory.php
@@ -2,7 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Endereco;
+use App\Models\Funcionario;
+use App\Models\Municipio;
+use App\Models\Provincia;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tecnico>
@@ -16,8 +22,37 @@ class TecnicoFactory extends Factory
      */
     public function definition(): array
     {
+        $user = User::create([
+            'name' => fake()->name(),
+            'email' => 'fulano@tec.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $provincia = Provincia::create([
+           'nome' => fake()->state(),
+        ]);
+
+        $municipio = Municipio::create([
+           'nome' => fake()->city(),
+           'provincia_id' => $provincia->id,
+        ]);
+
+        $endereco = Endereco::create([
+            'provincia_id' => $provincia->id,
+            'municipio_id' => $municipio->id,
+            'rua' => fake()->streetName(),
+        ]);
+
+        $funcionario = Funcionario::create([
+            'telefone' => fake()->phoneNumber(),
+            'user_id' => $user->id,
+            'endereco_id' => $endereco->id,
+            'estado_da_conta_id' => 1,
+        ]);
+
         return [
-            //
+            'funcionario_id' => $funcionario->id,
+            'especialidade_id' => 1,
         ];
     }
 }
