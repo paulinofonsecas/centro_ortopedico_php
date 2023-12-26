ac82489a342ca89a2ea1a4b52c4b29fcae8a37c0
Paulino Fonseca <paulinofonseca@gmail.com>
2023-12-26 05:05:06 +0100

    fix delete user

--- database/migrations/2023_08_03_152002_create_administradors_table.php
+++ database/migrations/2023_08_03_152002_create_administradors_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('administradors', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('funcionario_id')->constrained();
+            $table->foreignId('funcionario_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
