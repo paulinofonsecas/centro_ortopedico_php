1d5088f934b163a0df215cdde5e509e96c27e1b5
Paulino Fonseca <paulinofonsecass@gmail.com>
2023-12-28 15:32:04 +0100

    enable paciente view on tecnico

---
+++ app/Filament/Tecnico/Resources/PacienteResource/Pages/ViewPaciente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Tecnico\Resources\PacienteResource\Pages;
+
+use App\Filament\Medico\Resources\PacienteResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewPaciente extends ViewRecord
+{
+    protected static string $resource = PacienteResource::class;
+}
