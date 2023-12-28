fda6f3e0edfb80b262ea25c1cd7081552de7b3e7
Paulino Fonseca <paulinofonsecass@gmail.com>
2023-12-28 12:46:23 +0100

    altera a posicao dos modulos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -881,6 +881,24 @@ Filament::auth()->logout();
 
                     </a>
 
+                    <a href="http://ortopedico.test/tecnico/login"
+                        class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <div
+                                class="h-16 w-16  flex items-center justify-center rounded-full">
+
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Painel do Técnico</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Área reservada para Tecnicos e outros especialistas. Outros usuarios não serão aceites.
+                            </p>
+                        </div>
+
+
+                    </a>
+
                     <a href="http://ortopedico.test/medico/login"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div>
@@ -899,23 +917,7 @@ Filament::auth()->logout();
 
                     </a>
 
-                    <a href="http://ortopedico.test/tecnico/login"
-                        class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                        <div>
-                            <div
-                                class="h-16 w-16  flex items-center justify-center rounded-full">
-
-                            </div>
-
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Painel do Técnico</h2>
-
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Área reservada para Tecnicos e outros especialistas. Outros usuarios não serão aceites.
-                            </p>
-                        </div>
-
-
-                    </a>
+                    
                     
                     <a href="http://ortopedico.test/admin/login"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
